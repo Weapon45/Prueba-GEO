7d38a8a26b4eec297e3ceca7874ee4dc68600189
Weapon45 <luisorlando.itsx@gmail.com>
2016-10-28 15:30:01 -0500

    aafdsa

--- js/actions.js
+++ js/actions.js
@@ -1,12 +1,19 @@
-var fn{
-	ready: function(){
-		document.addEventListener("deviceready",fn.init,false);
-	},
-	init: function(){
-		navigator.geolocation.getCurrentPosition(fn.posicionObtenida,fn.error);
-	},
-	posicionObtenida: function(p){
-		alert(p.coords.latitude + " " + p.coords.logitude + " " + p.coords.timeStamp);
-	}
-}
-$(fn.posicionObtenida);
+var onSuccess = function(position) {
+        alert('Latitude: '          + position.coords.latitude          + '\n' +
+              'Longitude: '         + position.coords.longitude         + '\n' +
+              'Altitude: '          + position.coords.altitude          + '\n' +
+              'Accuracy: '          + position.coords.accuracy          + '\n' +
+              'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+              'Heading: '           + position.coords.heading           + '\n' +
+              'Speed: '             + position.coords.speed             + '\n' +
+              'Timestamp: '         + position.timestamp                + '\n');
+    };
+ 
+    // onError Callback receives a PositionError object 
+    // 
+    function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+    }
+ 
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
